18a3c8f8a36c3ed745ae37e1adcf552ef1a9e233
Abid Qadir <Abidumrani222@gmail.com>
2022-12-04 18:02:15 +0500

    moving

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
-let quantity = 1
-let price = 20
-let result = 0
-const btn1 =document.querySelector(".plus")
-const btn2 =document.querySelector(".minus")
 
-btn1.addEventListener("click",()=>{
-    quantity ++
-    result = price * quantity
-    document.querySelector(".quantity").innerHTML= quantity
-    document.querySelector(".price").innerText=result
+ 
+let btns = document.querySelectorAll("button")
+let user = "player1" 
+
+let btn1 
+let btn2
+let btn3
+let btn4
+let btn5
+let btn6
+let btn7
+let btn8
+let btn9
 
 
-})
+for(btn of btns){
 
-
-btn2.addEventListener("click",()=>{
-    if (quantity ==1)
+ btn.addEventListener("click", (b)=>{
+    if(user == "player1")
     {
-        document.querySelector(".quantity").innerHTML = quantity
-        document.querySelector(".price").innerHTML = price
+        if (btn1 == "X" && btn2 == "X"&& btn3 == "X" )
+        {
+            
+        b.target.innerHTML= "X"
+        user= "player"
+        document.getElementById("print").innerHTML = "X won"
+        
     }
-    else{
-
-        quantity -=1
-        result = result- price
-        document.querySelector(".quantity").innerHTML= quantity
-        document.querySelector(".price").innerText=result
+    }
+    else {
+        b.target.innerHTML ="0"
+        user = "player1"
         
     }
 
-
-})
+ })   
+}
