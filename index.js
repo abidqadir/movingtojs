2ff64ed7f4e4a7a0d7e6a1ab07b269ac3c336cfe
Abid Qadir <Abidumrani222@gmail.com>
2022-12-05 20:07:37 +0500

    setinterval

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-let student = {Name: "abid", Marks:96,sem:8,rollNo:"csc8f162" }
+/*let student = {Name: "abid", 
+Marks:96,
+sem:8,
+age:22,
+status:"single" ,
+address:{ city:"karachi",
+           postalacode:1234,
+            postalAdd:"new pnt colony near altamash dental institute karachi"    }
 
-console.log(student.rollNo)
+}
+
+
+    console.log(student,student.age,student.address.city)
+
+*/
+
+let sec =1
+function sayHello()
+
+
+{
+    console.log("wait biru", sec)
+    sec++
+
+    if(sec==6)
+    {
+        clearInterval(id)
+        console.log("wait is over 😂")
+    }
+    
+}
+
+let id = setInterval(sayHello,100)
